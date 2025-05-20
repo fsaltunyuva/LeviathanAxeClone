4b820b48aee34136899ca7f2f1419946a9b7b188
Furkan Safa Altunyuva <103141929+fsaltunyuva@users.noreply.github.com>
2025-05-20 15:21:26 +0300

    Dynamic Speed

--- Assets/Scripts/HorizontalAxeCaller.cs
+++ Assets/Scripts/HorizontalAxeCaller.cs
@@ -8,6 +8,9 @@ public class HorizontalAxeCaller : MonoBehaviour
     public Vector3 targetPosition; // Hand of Kratos
     public float speed = 1f;
     [SerializeField] private Transform kratosHand;
+    
+    public float baseSpeed = 1f;
+    [SerializeField] private float referenceDistance = 25f;
 
     private void Update()
     {
@@ -16,12 +19,19 @@ public class HorizontalAxeCaller : MonoBehaviour
             startPosition = transform.position; // Store the starting position
             interpolator = 0f; // Reset the interpolator to 0
             comeBack = true; // Start the slerp
+            Debug.Log("Distance: " + Vector3.Distance(startPosition, targetPosition));
         }
 
         targetPosition = kratosHand.position; // Get the target position every frame (Kratos' hand position changes because of animations)
 
         if (comeBack)
         {
+            float distance = Vector3.Distance(startPosition, targetPosition);
+
+            // Dynamic speed based on distance
+            float dynamicSpeed = baseSpeed * (referenceDistance / distance);
+            dynamicSpeed = Mathf.Clamp(dynamicSpeed, baseSpeed, baseSpeed * 5f); // Clamp the speed to a reasonable range
+            
             // Only slerp the X and Z axes
             Vector3 startXZ = new Vector3(startPosition.x, 0f, startPosition.z);
             Vector3 targetXZ = new Vector3(targetPosition.x, 0f, targetPosition.z);
@@ -33,7 +43,7 @@ public class HorizontalAxeCaller : MonoBehaviour
             // Combine the results (slerped XZ and lerped Y)
             transform.position = new Vector3(slerpedXZ.x, lerpedY, slerpedXZ.z);
 
-            interpolator += Time.deltaTime * speed; // Increase the interpolator based on speed
+            interpolator += Time.deltaTime * dynamicSpeed; // Increase the interpolator based on speed
             interpolator = Mathf.Clamp01(interpolator); // Clamp the interpolator to [0, 1] (to prevent overshooting)
             
             // transform.rotation = Quaternion.Slerp(transform.rotation, transform.rotation, 
